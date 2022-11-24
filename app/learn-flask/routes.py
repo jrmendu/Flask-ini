2018817d5e89a6ee662d23aac4745fb6962f9526
Pep <jrmendu@gmail.com>
2022-11-24 15:31:39 +0000

    Més routes. Passem variables i treballem amb jinja2

--- app/learn-flask/routes.py
+++ app/learn-flask/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, abort
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,12 +7,17 @@ app = Flask(__name__)
 @app.route("/hello/<name>")
 def hello(name = "pepitu"):
     return f"Hola {name}"
-    
+
 @app.route("/user")
-@app.route("/user/<string:user>")   # per si necessito rebre un string
-def user(user = "pepitu"):
+@app.route("/user/<string:user>")   # per si necessito rebre un string. Amb int es veu més clar l'exemple
+def user(user = ""):
     return f"Hola usuario {user}"
 
-# 5003
+@app.route("/products")
+@app.route("/products/<string:_category>")
+@app.route("/products/<string:_category>/<int:_id>")
+def products(_category = "", _id = 1):
+    return render_template("routes.html", category=_category, id=_id)
+
 if __name__ == '__main__':
    app.run(host="0.0.0.0", port=5003, debug = True)
